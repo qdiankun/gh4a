4176a9685b3352e8bd7d5df56e2a5ff9ae6af5de
Tunous <lukus178@gmail.com>
2017-06-20 18:34:21 +0200

    Fix error when there are pending reviews
    For now we can skip their comments as it's not possible to submit them from the app.

--- app/src/main/java/com/gh4a/loader/PullRequestCommentListLoader.java
+++ app/src/main/java/com/gh4a/loader/PullRequestCommentListLoader.java
@@ -47,16 +47,19 @@ public class PullRequestCommentListLoader extends IssueCommentListLoader {
             reviewsById.put(review.getId(), timelineReview);
             reviews.add(timelineReview);
 
-            if (Review.STATE_PENDING.equals(review.getState())) {
-                // For reviews with pending state we have to manually load the comments
-                List<CommitComment> pendingComments =
-                        pullRequestService.getReviewComments(repoId, mIssueNumber, review.getId());
-
-                for (CommitComment pendingComment : pendingComments) {
-                    CommitFile commitFile = filesByName.get(pendingComment.getPath());
-                    timelineReview.addComment(pendingComment, commitFile, true);
-                }
-            }
+            // FIXME: There's a bug on Github server side where HTTP error 500 is returned
+            //        when requesting review comments with reactions. Work around it by not
+            //        requesting comments for pending reviews for now.
+//            if (Review.STATE_PENDING.equals(review.getState())) {
+//                // For reviews with pending state we have to manually load the comments
+//                List<CommitComment> pendingComments =
+//                        pullRequestService.getReviewComments(repoId, mIssueNumber, review.getId());
+//
+//                for (CommitComment pendingComment : pendingComments) {
+//                    CommitFile commitFile = filesByName.get(pendingComment.getPath());
+//                    timelineReview.addComment(pendingComment, commitFile, true);
+//                }
+//            }
         }
 
         List<CommitComment> commitComments = pullRequestService.getComments(repoId, mIssueNumber);
